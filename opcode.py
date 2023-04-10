23f32169b08f049fedf36c31b07e3a5de7531bd5
Allen Shi <allen_shi2002@hotmail.com>
2023-04-09 21:52:25 -0400

    added part disassembler

--- opcode.py
+++ opcode.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from dataclasses import dataclass
 from typing import Literal
 import json
-import cartridge
+
 
 # Operand class
 @dataclass(frozen=True)
@@ -15,6 +15,7 @@ class Operand:
 
     def create(self, value):
         return Operand(immediate=self.immediate, name=self.name, bytes=self.bytes, value=value, adjust=self.adjust)
+
 
 # Instruction class
 @dataclass
@@ -31,33 +32,36 @@ class Instruction:
                            bytes=self.bytes, mnemonic=self.mnemonic)
 
 
-# Open instructions
-f = open('Opcodes.json')
-instructions = json.load(f)
+# Returns unprefixed and cbprefixed opcodes dictionary
+def getOpcodes(filename):
+    # Open instructions
+    f = open(filename)
+    instructions = json.load(f)
 
-# Initialize instruction list
-unprefixed = []
-cbprefixed = []
+    # Initialize instruction list
+    unprefixed = []
+    cbprefixed = []
 
-# Load instruction tables
-for ninstr in instructions["cbprefixed"]:
-    instr = instructions["cbprefixed"][ninstr]
-    oplist = []
-    for op in instr["operands"]:
-        operation = Operand(immediate=op["immediate"], name=op["name"], bytes=op.get("bytes"), value=None, adjust=None)
-        oplist.append(operation)
-    cbprefixed.append(
-        Instruction(opcode=ninstr, immediate=instr["immediate"], bytes=instr.get("bytes"), cycles=instr["cycles"],
-                    mnemonic=instr["mnemonic"], operands=oplist))
+    # Load instruction tables
+    for ninstr in instructions["cbprefixed"]:
+        instr = instructions["cbprefixed"][ninstr]
+        oplist = []
+        for op in instr["operands"]:
+            operation = Operand(immediate=op["immediate"], name=op["name"], bytes=op.get("bytes"), value=None,
+                                adjust=None)
+            oplist.append(operation)
+        cbprefixed.append(
+            Instruction(opcode=ninstr, immediate=instr["immediate"], bytes=instr.get("bytes"), cycles=instr["cycles"],
+                        mnemonic=instr["mnemonic"], operands=oplist))
 
-for ninstr in instructions["unprefixed"]:
-    instr = instructions["unprefixed"][ninstr]
-    oplist = []
-    for op in instr["operands"]:
-        operation = Operand(immediate=op["immediate"], name=op["name"], bytes=op.get("bytes"), value=None, adjust=None)
-        oplist.append(operation)
-    unprefixed.append(
-        Instruction(opcode=ninstr, immediate=instr["immediate"], bytes=instr.get("bytes"), cycles=instr["cycles"],
-                    mnemonic=instr["mnemonic"], operands=oplist))
-
-return ()
+    for ninstr in instructions["unprefixed"]:
+        instr = instructions["unprefixed"][ninstr]
+        oplist = []
+        for op in instr["operands"]:
+            operation = Operand(immediate=op["immediate"], name=op["name"], bytes=op.get("bytes"), value=None,
+                                adjust=None)
+            oplist.append(operation)
+        unprefixed.append(
+            Instruction(opcode=ninstr, immediate=instr["immediate"], bytes=instr.get("bytes"), cycles=instr["cycles"],
+                        mnemonic=instr["mnemonic"], operands=oplist))
+    return unprefixed, cbprefixed
